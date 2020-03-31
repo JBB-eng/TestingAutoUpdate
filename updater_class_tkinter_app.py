24025749340e46e6fd2bddb93d1300530c5d021b
JBB-eng <62054295+JBB-eng@users.noreply.github.com>
2020-03-31 02:53:34 +0200

    updated (working) autoupdater
    needs more work, but currently working

---
+++ updater_class_tkinter_app.py
@@ -0,0 +1,277 @@
+"""
+Creating classes and building tkinter app with updater
+"""
+
+__author__		= 'Jacob Bursavich'
+__copyright__	= 'Copyright (C) 2020, Jacob Bursavich'
+__credits__		= ['Jacob Bursavich']
+__license__		= 'The MIT License (MIT)'
+__version__		= '0.1'
+__maintainer__	= 'Jacob Bursavich'
+__email__		= 'jbursavich@gmail.com'
+__status__		= 'Beta'
+
+__AppName__		= 'JIAS Automation Assistant'
+
+
+#imports
+import tkinter as tk
+from tkinter import ttk, font, scrolledtext, filedialog, messagebox
+from PIL import ImageTk, Image, ImageOps
+from urllib.request import urlopen
+from MessageBox import *
+import os, webbrowser, cgi, threading, ctypes
+from ctypes import c_int, WINFUNCTYPE, windll
+from ctypes.wintypes import HWND, LPCWSTR, UINT
+
+
+prototype = WINFUNCTYPE(c_int, HWND, LPCWSTR, LPCWSTR, UINT)
+paramflags = (1, "hwnd", 0), (1, "text", "Hi"), (1, "caption", "Hello from ctypes"), (1, "flags", 0)
+MessageBox = prototype(("MessageBoxW", windll.user32), paramflags)
+
+tab_names = ["New MS", "Revised MS", "Extra Tab", "Blah Blah..."] #add more to increase amount of tabs
+tabs = [None]*len(tab_names) #holds the tab variables
+
+
+class Main:
+	def __init__(self, parent):
+		def CheckUpdates():
+			#check if __version__ is lower than latest release
+			try:
+				url_data = urlopen("http://raw.githubusercontent.com/JBB-eng/TestingAutoUpdate/master/Version")
+				latest_version = str(url_data.read(), 'utf-8')
+				if __version__ < latest_version:
+					mb = MessageBox(None,__AppName__+' '+ str(__version__)+' needs to update to version '+str(latest_version),'Update Available',flags.MB_YESNO | flags.MB_ICONQUESTION)
+					if mb ==  6:
+						print("picked YES")
+						CallUpdateManager = UpdateManager(parent)
+						pass
+					elif mb == 7:
+						print("Picked NO")
+						pass
+				else:
+					messagebox.showinfo('Software Update','No Updates are Available.')
+			except Exception as e:
+				messagebox.showinfo('Software Update','Unable to Check for Update, Error:' + str(e))
+				#CallUpdateManager = UpdateManager(parent)
+
+
+		def AboutMe():
+			#loads info
+			CallDisplayAboutMe = DisplayAboutMe(parent)
+			pass
+
+		def runBinary():
+			#runs an .exe file
+			pass
+
+		def UpdateUsingManager():
+			#data = urllib
+			#another update version
+			pass
+
+		def StartApp():
+			CheckUpdates()
+			menubar = tk.Menu(parent)
+			filemenu = tk.Menu(menubar, tearoff=0)
+			filemenu.add_command(label='Exit', command=parent.destroy)
+			menubar.add_cascade(label='File', menu=filemenu)
+			helpmenu = tk.Menu(menubar, tearoff=0)
+			helpmenu.add_command(label='Check For Updates', command=CheckUpdates)
+			helpmenu.add_command(label='About', command=AboutMe)
+			menubar.add_cascade(label='Help', menu=helpmenu)
+			parent.config(menu=menubar)
+
+			rows = 0
+			while rows < 50:
+				parent.rowconfigure(rows, weight=1)
+				parent.columnconfigure(rows, weight=1)
+				rows += 1
+
+			#Setup for Tkinter tabs in the main window
+			nb = ttk.Notebook(parent)
+			nb.grid(row=1, column=1, columnspan=48, rowspan=49, sticky='NESW')
+
+			for i in range(len(tabs)):
+				tabs[i] = ttk.Frame(nb)
+				rows = 0
+				while rows < 50:
+					tabs[i].rowconfigure(rows, weight=1)
+					tabs[i].columnconfigure(rows, weight=1)
+					rows += 1
+				nb.add(tabs[i], text=tab_names[i])
+
+			#begins the tkinter gui application
+			pass
+		StartApp()
+
+
+class UpdateManager(tk.Toplevel):
+	def __init__(self, parent):
+		tk.Toplevel.__init__(self, parent)
+
+		self.transient(parent)
+		self.result = None
+		self.grab_set()
+		w = 350; h = 200
+		sw = self.winfo_screenwidth()
+		sh = self.winfo_screenheight()
+		x = (sw - w)/2
+		y = (sh - h)/2
+		self.geometry('{0}x{1}+{2}+{3}'.format(w, h, int(x), int(y)))
+		self.resizable(width=False, height=False)
+		self.title('Update Manager')
+		self.wm_iconbitmap('robot.ico')
+
+		image = Image.open('update.png')
+		photo = ImageTk.PhotoImage(image)
+		label = tk.Label(self, image=photo)
+		label.image = photo
+		label.pack()
+		#label.grid(column=0, row=0)
+
+		def StartUpdateManager():
+			#starts the download of the newer version and updates progress bar
+			try:
+				f=open(self.tempdir+'/'+self.appname,'wb')
+				while True:
+					self.newdata = self.data.read(self.chunk)
+					#print("self.newdata:", self.newdata)
+					#self.str_newdata = self.newdata.format(self.newdata).encode()
+					if self.newdata:
+						#print('1')
+						f.write(self.newdata)
+						#print('2')
+						self.downloadeddata += self.newdata
+						#print('3')
+						self.progressbar['value'] = len(self.downloadeddata)
+					else:
+						break
+			except Exception as e:
+				messagebox.showerror('Error',str(e))
+				self.destroy()
+			else:
+				f.close()
+				self.button1.config(text='Update App!', state=tk.NORMAL)
+				self.label1.config(text='Success!')
+							
+
+		def InstallUpdate():
+			#installs update
+			#runs the downloaded newer version of the app
+			#then destroy() this current version of the app
+
+			#all future versions will also check their local working directory for early binary versions
+			#of the app, and then delete them.  this will occur when the app is started.
+			#also, all of the app binary files will have the following format:
+			#[name of application]_v[version number].exe
+			#for example: "JIASAutomationAssistant_v0.3.exe"
+			pass
+
+		#params = cgi.parse_header(self.data.headers.get('Content-Disposition', ''))
+		#filename = params[-1].get('filename')
+		#self.appname = filename
+		#self.tempdir = os.environ.get('temp')
+		#self.chunk = 1048576
+
+		try:
+			self.data = urlopen("https://github.com/JBB-eng/TestingAutoUpdate/releases/download/0.2/JIAS-Automation_build0_1.exe")
+			self.filesize = cgi.parse_header(self.data.headers.get('Content-Length', ''))[0]
+
+			params = cgi.parse_header(self.data.headers.get('Content-Disposition', ''))
+			filename = params[-1].get('filename')
+			self.appname = filename
+			#self.tempdir = os.environ.get('temp')
+			self.tempdir = os.getcwd()
+			print('temp folder:', self.tempdir)
+			self.chunk = 1048576
+								
+		except Exception as e:
+			messagebox.showerror('Error', str(e))
+			self.destroy()
+		else:
+			#self.downloadeddata = ''
+			self.downloadeddata = b''
+			self.progressbar = ttk.Progressbar(self,
+									orient='horizontal',
+									length=200,
+									mode='determinate',
+									value=0,
+									#maximum=self.data.info().getheader('Content-Length').strip())len(data.read())
+									#maximum=len(self.data))
+									maximum=self.filesize)
+			self.label1 = ttk.Label(self, text="Please wait while update is downloaded")
+			self.label1.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+			self.progressbar.place(relx=0.5, rely=0.4, anchor=tk.CENTER)
+			self.button1 = ttk.Button(self, text='Update App', state=tk.DISABLED, command=InstallUpdate)
+			self.button1.place(relx=0.50, rely=0.8, anchor=tk.CENTER)
+		
+
+		self.t1 = threading.Thread(target=StartUpdateManager)
+		self.t1.start()	
+
+
+
+class DisplayAboutMe(tk.Toplevel):
+	def __init__(self, parent):
+		tk.Toplevel.__init__(self, parent)
+
+		self.transient(parent)
+		self.result = None
+		self.grab_set()
+		w = 285; h = 273
+		sw = self.winfo_screenwidth()
+		sh = self.winfo_screenheight()
+		x = (sw - w)/2
+		y = (sh - h)/2
+		self.geometry('{0}x{1}+{2}+{3}'.format(w, h, int(x), int(y)))
+		self.resizable(width=False, height=False)
+		self.title('About')
+		self.wm_iconbitmap('robot.ico')
+
+		self.image = Image.open('jias_robot1.png')
+		self.size = (100, 100)
+		self.thumb = ImageOps.fit(self.image, self.size, Image.ANTIALIAS)
+		self.photo = ImageTk.PhotoImage(self.thumb)
+		logoLabel = tk.Label(self, image=self.photo); logoLabel.pack(side=tk.TOP, pady=10)
+
+		f1 = tk.Frame(self); f1.pack()
+		f2 = tk.Frame(self); f2.pack(pady=10)
+		f3 = tk.Frame(f2); f3.pack()
+
+		def CallHyperLink(EventArgs):
+			webbrowser.open_new_tab('https://ch.linkedin.com/in/jacob-bursavich')
+		
+		tk.Label(f1, text=__AppName__+' '+str(__version__)).pack()
+		tk.Label(f1, text='Copyright (C) 2020 Jacob Bursavich').pack()
+		tk.Label(f1, text='All rights reserved').pack()
+
+		f = font.Font(size=10, slant='italic', underline=True)
+		label1 = tk.Label(f3, text='jbursavich', font = f, cursor='hand2')
+		label1['foreground'] = 'blue'
+		label1.pack(side=tk.LEFT)
+		label1.bind('<Button-1>', CallHyperLink)
+		ttk.Button(self, text='OK', command=self.destroy).pack(pady=5)
+
+
+
+def main():
+	root = tk.Tk()
+	root.title(__AppName__+' '+str(__version__))
+	w=650; h=400
+	sw = root.winfo_screenwidth()
+	sh = root.winfo_screenheight()
+	x = (sw - w) / 2
+	y = (sh - h) / 2
+	root.geometry('{0}x{1}+{2}+{3}'.format(w, h, int(x), int(y)))
+	root.resizable(width=False, height=False)
+	root.wm_iconbitmap('robot.ico')
+	win = Main(root)
+	root.mainloop()	
+
+
+if __name__ == '__main__':
+	main()
+
+
+
